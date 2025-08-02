7175ed839f59d628f3db8397731bc77ed95b6e23
Mustafa Dawood <khanmustafadawood555@gmail.com>
2025-08-03 03:51:36 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let Quotes = [
+  '"Stay hungry, stay foolish."',
+  '"Success is not final, failure is not fatal: It is the courage to continue that counts."',
+  '"It always seems impossible until it’s done."',
+  '"Don’t watch the clock; do what it does. Keep going."',
+  '"Your time is limited, so don’t waste it living someone else’s life."',
+  '"If you can’t fly, then run. If you can’t run, then walk. If you can’t walk, then crawl. But by all means, keep moving."',
+  '"Code is like humor. When you have to explain it, it’s bad."',
+  '"The best way to get started is to quit talking and begin doing."',
+  '"Whether you think you can or you think you can’t, you’re right."',
+];
+let Authors = [
+  "— Steve Jobs",
+  "— Winston Churchill",
+  "— Nelson Mandela",
+  "— Sam Levenson",
+  "— Richard Branson",
+  "— Steve Jobs",
+  "— Martin Luther King Jr.",
+  "— Cory House",
+  "— Walt Disney",
+  "— Henry Ford",
+];
+
+let para = document.querySelector(".quote");
+let author = document.querySelector(".author");
+let body=document.querySelector("body");
